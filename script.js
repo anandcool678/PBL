8324a4db29eef4ec679730edc52563ace20681fe
Anand Prakash Dwivedi <anand201101dwivedi@gmail.com>
2021-06-10 23:34:07 +0530

    json file added

--- script.js
+++ script.js
@@ -29,3 +29,28 @@ window.onload= function () {
      plusSlides(1);
  }, 3000);
  }
+
+ fetch('data.json')
+ .then(function (response) {
+     return response.json();
+ })
+ .then(function (data) {
+     appendData(data);
+ })
+ .catch(function (err) {
+     console.log('error: ' + err);
+ });
+ function appendData(data) {
+  var mainContainer = document.getElementById("search");
+  for (var i = 0; i < data.length; i++) {
+    if(mainContainer === data[i].Aadhar){
+      var div = document.createElement("div");
+      div.innerHTML = 'Name: ' + data[i].Name;
+      mainContainer.appendChild(div);
+    }
+  
+      
+  }
+}
+  
+  
